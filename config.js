08c377634accfabd752a656f239d4bb21fbd0fa8
Leland Moy <Leland.d.moy@gmail.com>
2021-07-10 01:55:20 -0500

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,10 @@
  */
 
 require('dotenv').config();
-// mongodb+srv://Tracker:Pz0VAqfeoeDy9hBz@tracker.sbezj.mongodb.net/Tracker?retryWrites=true&w=majority&ssl=true // process.env.MONGOPATH
-// mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false
+
 module.exports = { 
     "token" : process.env.BOTTOKEN,
     "ownerToken" : process.env.OWNERTOKEN,
-    "prefix" : "!",
+    "prefix" : "!", 
     "mongoPath" : "mongodb://localhost:27017/Tracker",
 };
