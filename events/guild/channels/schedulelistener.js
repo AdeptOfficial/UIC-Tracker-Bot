71d5212aa8e2e70e4e697ebd6cf32d9ab9ae6229
Leland Moy <Leland.d.moy@gmail.com>
2021-07-30 18:16:51 -0500

    Update schedulelistener.js

--- events/guild/channels/schedulelistener.js
+++ events/guild/channels/schedulelistener.js
@@ -1,5 +1,5 @@
-                  // testing grounds,      
-const channels = ['870788489770328177']
+                  // testing grounds,      UIC Tracker
+const channels = ['870788489770328177', '870476959946268712']
 require('module-alias/register')
 const { schedules } = require('@Util/school/schedules.json') 
 const addSchedule = require('@Util/school/addSchedule.js') 
