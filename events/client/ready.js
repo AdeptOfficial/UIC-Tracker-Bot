270ffa632de03b0bcf897a28267bde252e6e5e39
Leland Moy <Leland.d.moy@gmail.com>
2021-08-16 23:09:59 -0500

    Minor update
    thanksHelper (constant reminder is a no more - cooldown: 60 min)
    
    added new packages for future updates

--- events/client/ready.js
+++ events/client/ready.js
@@ -4,10 +4,8 @@
  * @file ready.js
  * @info readys the bot, handles connections with database
  */
-
 const mongo = require('./mongo');
 const thanksHelper = require('./thanksHelper');
-
 
 require('module-alias/register')
 module.exports = (client, Discord, message) => {
--- events/client/thanksHelper.js
+++ events/client/thanksHelper.js
@@ -1,7 +1,20 @@
 module.exports = (client) => {
+    const cooldown = new Set();    
+    let hour = 60000 * 60
     client.on('message', function(msg) {
-        if(msg.content.startsWith('thanks') || msg.content.startsWith('thx') || msg.content.startsWith('thnks') || msg.content.startsWith('ty')) {
-            msg.reply('Consider thanking the person by using !thank @user :D')
+        if (cooldown.has(client.user.id)) {
+            
+        } else {
+            if (msg.content.startsWith('thanks') || msg.content.startsWith('thx') || msg.content.startsWith('thnks') || msg.content.startsWith('ty')) {
+                msg.reply('Consider thanking the person by using !thank @user :D')
+            }
+            console.log('thank reminder used')
+            cooldown.add(client.user.id)
+            setTimeout(() => {
+                // Removes the user from the set after a minute
+                cooldown.delete(client.user.id);
+                console.log('thank reminder reset')
+              }, hour);
         }
     })
 }
